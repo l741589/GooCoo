35019df3e62972f276e3f89e8d72cafd961e421b
tjulinfan <tjulinfan@gmail.com>
2013-07-24 15:34:33 +0800

    ASP.NET MVC 3 proj

---
+++ GooCooWeb/Controllers/PersonInfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GooCooWeb.Controllers
+{
+    public class PersonInfoController : Controller
+    {
+        //
+        // GET: /PersonInfo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
