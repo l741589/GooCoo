f98d3014d5c5764fa31a00f7e84d9894f804107d
Roy Luo <459735496@qq.com>
2013-07-26 14:23:09 +0800

    After Merge OYF

---
+++ GooCooServer/DAOtest/test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using GooCooServer.DAO;
+using GooCooServer.IDAO;
+
+namespace GooCooServer.DAOtest
+{
+    class test
+    {
+        public static void Main()
+        {
+            IBook_BookInfoDAO tester = (IBook_BookInfoDAO)DAOFactory.createDAO("Book_BookInfoDAO");
+            tester.testinsert();
+            Console.ReadLine();
+        }
+    }
+}
