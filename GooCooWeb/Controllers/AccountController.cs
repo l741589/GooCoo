34cf7a3b04ff44ddda6031162753ddcdf34ba264
tjulinfan <tjulinfan@gmail.com>
2013-07-27 20:36:55 +0800

    push 20130727_2036 by LF

---
+++ GooCooWeb/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GooCooWeb.Models;
+using GooCooServer.Exception;
+
+namespace GooCooWeb.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnModel model, String returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var sessionID = ValidateUser(model.Id, model.Password);
+                    Session["UserSessionID"] = sessionID;
+                    if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                        && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                catch (BMException ex)
+                {
+                    ModelState.AddModelError("", "提供的用户名或密码不正确。");
+                }
+            }
+
+            // 如果我们进行到这一步时某个地方出错，则重新显示表单
+            return View(model);
+        }
+
+        private String ValidateUser(String id, String password)
+        {
+            return "";
+        }
+
+    }
+}
---
+++ GooCooWeb/Views/PersonalInfo/BorrowInfo.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/FourAreasLayout.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="TitleContent" ContentPlaceHolderID="TitleContent" runat="server">
+    借阅列表
+</asp:Content>
+
+<asp:Content ID="OtherCss" ContentPlaceHolderID="OtherCssStyle" runat="server">
+</asp:Content>
+
+<asp:Content ID="LeftNavbar" ContentPlaceHolderID="LeftNavbar" runat="server">
+    <% Html.RenderPartial("LeftNavbar"); %>
+</asp:Content>
+
+<asp:Content ID="MainContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row-fluid">
+    </div>
+</asp:Content>
+
+
+<asp:Content ID="Footer" ContentPlaceHolderID="Footer" runat="server">
+    <% Html.RenderPartial("~/Views/Shared/Footer.ascx"); %>
+</asp:Content>
+
+<asp:Content ID="Content6" ContentPlaceHolderID="OtherJavascript" runat="server">
+    <script type="text/javascript">
+        document.getElementById("nav_borrowInfo").className = "active";
+    </script>
+</asp:Content>
