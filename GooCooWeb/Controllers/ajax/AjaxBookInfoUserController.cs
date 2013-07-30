0a1143917ffcddcdf215a0c739f753a6c5e282ca
wxy325 <wxy325@qq.com>
2013-07-31 00:03:41 +0800

    ajax测试成功，增加post限制

--- GooCooWeb/Controllers/ajax/AjaxBookInfoUserController.cs
+++ GooCooWeb/Controllers/ajax/AjaxBookInfoUserController.cs
@@ -9,23 +9,28 @@ using GooCooServer.Entity;
 
 namespace GooCooWeb.Controllers.ajax
 {
+    //地址AjaxBookInfoUser/AddFavor?isbn=aaa
     public class AjaxBookInfoUserController : Controller
     {
         //
-        // GET: /AjaxBookInfoUser/                
+        // GET: /AjaxBookInfoUser/   
+        [HttpPost]     
         public ActionResult AddFavor(string isbn)
         {
             return this.AddRelation(isbn, User_BookInfo.ERelation.FAVOR, Operation.ADD);
         }
+        [HttpPost]
         public ActionResult AddOrder(string isbn)
         {
             return this.AddRelation(isbn, User_BookInfo.ERelation.ORDER, Operation.ADD);
         }
 
+        [HttpPost]
         public ActionResult RemoveOrder(string isbn)
         {
             return this.AddRelation(isbn, User_BookInfo.ERelation.ORDER, Operation.REMOVE);
         }
+        [HttpPost]
         public ActionResult RemoveFavor(string isbn)
         {
             return this.AddRelation(isbn, User_BookInfo.ERelation.FAVOR, Operation.REMOVE);
