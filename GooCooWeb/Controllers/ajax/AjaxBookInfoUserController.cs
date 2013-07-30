0346099f00d6552635174bf23e32097f4d5e5592
wxy325 <wxy325@qq.com>
2013-07-30 23:31:26 +0800

    添加Ajax收藏、预定，还未测试

--- GooCooWeb/Controllers/ajax/AjaxBookInfoUserController.cs
+++ GooCooWeb/Controllers/ajax/AjaxBookInfoUserController.cs
@@ -3,18 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GooCooServer.IDAO;
+using GooCooServer.DAO;
+using GooCooServer.Entity;
 
 namespace GooCooWeb.Controllers.ajax
 {
     public class AjaxBookInfoUserController : Controller
     {
         //
-        // GET: /AjaxBookInfoUser/
-
-        public ActionResult Index()
+        // GET: /AjaxBookInfoUser/                
+        public ActionResult AddFavor(string isbn)
         {
-            return View();
+            return this.AddRelation(isbn, User_BookInfo.ERelation.FAVOR, Operation.ADD);
+        }
+        public ActionResult AddOrder(string isbn)
+        {
+            return this.AddRelation(isbn, User_BookInfo.ERelation.ORDER, Operation.ADD);
         }
 
+        public ActionResult RemoveOrder(string isbn)
+        {
+            return this.AddRelation(isbn, User_BookInfo.ERelation.ORDER, Operation.REMOVE);
+        }
+        public ActionResult RemoveFavor(string isbn)
+        {
+            return this.AddRelation(isbn, User_BookInfo.ERelation.FAVOR, Operation.REMOVE);
+        }
+
+
+
+        private enum Operation { ADD, REMOVE }
+        private ActionResult AddRelation(string isbn, User_BookInfo.ERelation relation, Operation operation)
+        {
+            try
+            {
+                IUserDAO userDAO = DAOFactory.createDAO("UserDAO") as IUserDAO;
+                IUser_BookInfoDAO userBookInfoDAO = DAOFactory.createDAO("User_BookInfoDAO") as IUser_BookInfoDAO;
+
+                string userSessionID = (string)Session["UserSessionID"];
+                User user = userDAO.Get(userSessionID);
+                if (operation == Operation.ADD)
+                {
+                    userBookInfoDAO.Add(isbn, user.Id, relation);
+                }
+                else
+                {
+                    userBookInfoDAO.Del(isbn, user.Id, relation);
+                }
+                return Json(new { result = true });
+            }
+            catch (Exception)
+            {
+                return Json(new { result = false });
+            }
+        }
     }
 }
